076648b4263cc1a5c59f0243056e45a865e1bc3b
NieamulKabir <kabir000079@gmail.com>
2024-07-01 11:26:41 +0600

    taskSlice and functionality added

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -2,11 +2,18 @@ import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import MyTasks from "../components/tasks/MyTasks";
 import TaskCard from "../components/tasks/TaskCard";
 import { useState } from "react";
-import Modal from "../components/ui/Modal";
+
 import AddTaskModal from "../components/tasks/AddTaskModal";
+import { useSelector } from "react-redux";
 
 const Tasks = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { tasks } = useSelector((state) => state.tasks);
+
+  const runningTasks= tasks.filter((item) => item.status === "running");
+  const pendingTasks= tasks.filter((item) => item.status === "pending");
+  const completeTasks= tasks.filter((item) => item.status === "done");
+
   return (
     <div className="h-screen grid grid-cols-12">
       <div className="col-span-9 px-10 pt-10">
@@ -56,34 +63,39 @@ const Tasks = () => {
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>Up Next</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                0
+                {pendingTasks.length}
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
+              {pendingTasks.map((item) => (
+                <TaskCard key={item.id} task={item} />
+              ))}
             </div>
           </div>
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
               <h1>In Progress</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                0
+                {runningTasks.length}
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
-              <TaskCard />
+              {runningTasks.map((item) => (
+                <TaskCard key={item.id} task={item} />
+              ))}
             </div>
           </div>
           <div className="relative h-[800px] overflow-auto">
             <div className="flex sticky top-0 justify-between bg-[#D3DDF9] p-5 rounded-md mb-3">
-              <h1>Up Next</h1>
+              <h1>Complete</h1>
               <p className="bg-primary text-white w-6 h-6 grid place-content-center rounded-md">
-                0
+                {completeTasks.length }
               </p>
             </div>
             <div className="space-y-3">
-              <TaskCard />
+              {completeTasks.map((item) => (
+                <TaskCard key={item.id} task={item} />
+              ))}
             </div>
           </div>
         </div>
--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -10,14 +10,25 @@ const tasksSlice = createSlice({
   reducers: {
     addTask: (state, { payload }) => {
       if (state.tasks.length === 0) {
-        state.tasks.push({ id: 1, ...payload });
+        state.tasks.push({ id: 1, status: "pending", ...payload });
       } else {
         const lastElement = state.tasks.at(-1);
-        state.tasks.push({ id: lastElement.id + 1, ...payload });
+        state.tasks.push({
+          id: lastElement.id + 1,
+          status: "pending",
+          ...payload,
+        });
       }
+    },
+    removeTask: (state, { payload }) => {
+      state.tasks.filter((item) => item.id !== payload);
+    },
+    updateStatus: (state, { payload }) => {
+      const target = state.tasks.find((item) => item.id === payload.id);
+      target.status =payload.status;
     },
   },
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, removeTask ,updateStatus } = tasksSlice.actions;
 export default tasksSlice.reducer;
