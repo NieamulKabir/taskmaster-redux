3b09857233e6b8d1515be410d1c41ec381ca621d
NieamulKabir <kabir000079@gmail.com>
2024-07-01 11:52:24 +0600

    update slice

--- src/redux/features/tasks/tasksSlice.js
+++ src/redux/features/tasks/tasksSlice.js
@@ -21,14 +21,14 @@ const tasksSlice = createSlice({
       }
     },
     removeTask: (state, { payload }) => {
-      state.tasks.filter((item) => item.id !== payload);
+      state.tasks = state.tasks.filter((item) => item.id !== payload);
     },
     updateStatus: (state, { payload }) => {
       const target = state.tasks.find((item) => item.id === payload.id);
-      target.status =payload.status;
+      target.status = payload.status;
     },
   },
 });
 
-export const { addTask, removeTask ,updateStatus } = tasksSlice.actions;
+export const { addTask, removeTask, updateStatus } = tasksSlice.actions;
 export default tasksSlice.reducer;
