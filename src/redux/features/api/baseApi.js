0ba84c4942fcd030c41e91c6d23e0bed4d7fb1d0
NieamulKabir <kabir000079@gmail.com>
2024-07-06 21:14:24 +0600

    update

--- src/redux/features/api/baseApi.js
+++ src/redux/features/api/baseApi.js
@@ -3,9 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5173/",
+    baseUrl: "http://localhost:5173",
   }),
-  endpoints: {},
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+    }),
+  }),
 });
 
+export const { useGetPostsQuery } = baseApi;
 export default baseApi;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksSlice from "./features/tasks/tasksSlice";
 import userSlice from "./features/user/userSlice";
+import baseApi from "./features/api/baseApi";
 
 export const store = configureStore({
   reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
     tasks: tasksSlice,
-    users:userSlice
+    users: userSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
